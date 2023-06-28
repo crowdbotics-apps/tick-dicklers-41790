869fa6b0fa8d82a5d323c477e0528cf3eb533dd9
Crowdbotics <team@crowdbotics.com>
2023-06-28 19:47:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tick-dicklers-41790",
+  url: "https://tick-dicklers-41790.botics.co",
+  api: "https://tick-dicklers-41790.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
